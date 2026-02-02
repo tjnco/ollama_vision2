9fc2205a6712641d17562fc796c56b08de396b7f
Tyler McClain <github@tjnco.net>
2026-02-01 16:53:02 -0800

    syntwx fix.

--- custom_components/ollama_vision/__init__.py
+++ custom_components/ollama_vision/__init__.py
@@ -205,7 +205,7 @@ async def handle_analyze_image(hass, call):
                 image_urls = [image_url]
         else:
             image_urls = [image_url]
-    else if isinstance(image_url, list):
+    elif isinstance(image_url, list):
         image_urls = image_url
     else:
         image_urls = list(image_url)
